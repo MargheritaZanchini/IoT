522b981d31113e636a152d8db9f2cc65fc04d96f
Marco "Volpo" Marrelli <76500649+marcomarrelli@users.noreply.github.com>
2024-11-13 17:14:03 +0100

    chore: Init Assignment02
    Initialized Assignment 02
    Arduino + PC (Java)

---
+++ assignment02/pc/src/main/java/assignment02/App.java
@@ -0,0 +1,9 @@
+package assignment02;
+
+import javafx.application.Application;
+
+public final class App {
+    public static void main(final String[] args) {
+        Application.launch(Launcher.class, args);
+    }
+}
---
+++ assignment02/pc/src/main/java/assignment02/Launcher.java
@@ -0,0 +1,53 @@
+package assignment02;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+public class Launcher extends Application {
+    private static final String APPLICATION_NAME = "OPERATOR DASHBOARD";
+    private static final String APPLICATION_LAYOUT = "Main.fxml";
+    private static final boolean APPLICATION_RESIZABLE = false;
+
+    private final Function<Integer, Void> CLOSE_FUNCTION = (input) -> {
+        Platform.exit();
+        System.exit(input);
+        return null;
+    };
+
+    private final FXMLLoader LOADER = new FXMLLoader(getClass().getResource(APPLICATION_LAYOUT));
+    private Parent root;
+    private Scene scene;
+
+    private Controller controller;
+
+    @Override
+    public void start(final Stage primaryStage) {
+        try {
+            root = LOADER.load();
+        }
+        catch (IOException error) {
+            System.err.println("File FXML Not Found! " + error.getMessage());
+            CLOSE_FUNCTION.apply(-1);
+        }
+
+        scene = new Scene(root);
+        controller = LOADER.getController();
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(APPLICATION_NAME);
+        primaryStage.setResizable(APPLICATION_RESIZABLE);
+        primaryStage.setOnCloseRequest((closeRequest) -> CLOSE_FUNCTION.apply(0));
+    
+        controller.setToxicWasteLevel(0.90);
+
+        primaryStage.show();
+    }
+}
