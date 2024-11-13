1079a9f02c3d168f011ce6eb27aefbe227c0d023
Marco "Volpo" Marrelli <76500649+marcomarrelli@users.noreply.github.com>
2024-11-13 17:27:04 +0100

    chore: Initialized assignment02 Directory

---
+++ assignment02/src/pc/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module assignment02 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    
+    opens assignment02 to javafx.fxml;
+    exports assignment02;
+}
