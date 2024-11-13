8c7a9c881347b179403bcbda2e303a3a86366c5e
Marco "Volpo" Marrelli <76500649+marcomarrelli@users.noreply.github.com>
2024-11-13 17:26:31 +0100

    revert: Deleted assignment02 Directory

--- assignment02/pc/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module assignment02 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    
-    opens assignment02 to javafx.fxml;
-    exports assignment02;
-}
