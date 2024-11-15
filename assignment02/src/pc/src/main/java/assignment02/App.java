e292dcec7ac4548736bb1b497d89225816705627
Marco "Volpo" Marrelli <76500649+marcomarrelli@users.noreply.github.com>
2024-11-15 12:01:04 +0100

    chore: Deleted assignment02 Directory

--- assignment02/src/pc/src/main/java/assignment02/App.java
+++
@@ -1,9 +0,0 @@
-package assignment02;
-
-import javafx.application.Application;
-
-public final class App {
-    public static void main(final String[] args) {
-        Application.launch(Launcher.class, args);
-    }
-}
