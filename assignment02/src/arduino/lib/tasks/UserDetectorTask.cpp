53d960daf0c74dd8ef3c7fb5a90e808d53ed1308
Margherita <margherita.zanchini@studio.unibo.it>
2024-11-26 19:05:41 +0100

    FIX: fixed SLEEP mode in UserDetectorTask

--- assignment02/src/arduino/lib/tasks/UserDetectorTask.cpp
+++ assignment02/src/arduino/lib/tasks/UserDetectorTask.cpp
@@ -45,18 +45,10 @@ void UserDetectorTask::tick(){
         Serial.println("Sono in SLEEP");
         set_sleep_mode(SLEEP_MODE_IDLE);
         sleep_enable();
-
-        power_adc_disable();
-        power_spi_disable();
-        power_timer0_disable();
-        power_timer1_disable();
-        power_timer2_disable();
-        power_twi_disable();  
-        /* Now enter sleep mode. */
-        sleep_mode();  
-        /* The program will continue from here after the timer timeout*/
-        sleep_disable(); /* First thing to do is disable sleep. */
-        /* Re-enable the peripherals. */
+        power_all_disable();
+        sleep_mode();   
+        /* back */
+        sleep_disable();
         power_all_enable();
 
         _lastDetectedTime = 0;
