40750861a99c8f8f89c3bb6a14ed7f176de44a62
marcomarrelli <marcomarrelli03@gmail.com>
2025-02-19 17:24:02 +0100

    doc: Added Comments

--- assignment03/src/control-unit-backend/cu/src/main/java/control/unit/TemperatureManager.java
+++ assignment03/src/control-unit-backend/cu/src/main/java/control/unit/TemperatureManager.java
@@ -4,23 +4,42 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * TemperatureManager Class
+ */
 public class TemperatureManager {
     private final List<Double> temperatures;
 
-    public final static float T1 = 22;
-    public final static float T2 = 28;
+    public final static float T1 = 22; /** First Temperature Alert */
+    public final static float T2 = 28; /** Second Temperature Alert */
 
+    /**
+     * TemperatureState Enum
+     * 
+     * @see #NORMAL
+     * @see #HOT
+     * @see #TOO_HOT
+     * @see #ALARM
+     */
     public static enum TemperatureState {
-        NORMAL,
-        HOT,
-        TOO_HOT,
-        ALARM
+        NORMAL, /** Normal */
+        HOT, /** Hot */
+        TOO_HOT, /** Too Hot */
+        ALARM /** Alarm */
     }
 
+    /**
+     * TemperatureManager Constructor
+     */
     public TemperatureManager() {
         this.temperatures = new ArrayList<>();
     }
 
+    /**
+     * Add Temperature to Queue
+     * 
+     * @param temperature Temperature
+     */
     public void addTemperature(double temperature) {
         if(this.temperatures.size() >= 10) {
             this.temperatures.remove(0);
@@ -28,6 +47,12 @@ public class TemperatureManager {
         this.temperatures.add(temperature);
     }
 
+    /**
+     * Get Temperature by Index
+     * 
+     * @param index Index
+     * @return Temperature
+     */
     public double getTemperature(int index) {
         if(this.temperatures.isEmpty()) { // No temperatures
             return -1;
@@ -39,27 +64,71 @@ public class TemperatureManager {
         return this.temperatures.get(index);
     }
 
+    /**
+     * Get Current Temperature
+     * 
+     * @return Current Temperature
+     */
     public double getCurrentTemperature() {
         return this.getTemperature(this.temperatures.size()-1);
     }
 
+    /**
+     * Get Temperatures
+     * 
+     * @return Temperature List
+     * @see #temperatures
+     */
     public List<Double> getTemperatures() {
         return List.copyOf(this.temperatures);
     }
 
+    /**
+     * Get Minimum Temperature
+     * 
+     * @return Minimum Temperature
+     * @see Collections#min
+     */
     public double getMinTemperature() {
         return Collections.min(this.temperatures);
     }
 
+    /**
+     * Get Average Temperature
+     * 
+     * @return Average Temperature
+     */
     public double getAverageTemperature() {
         return this.temperatures.stream().mapToDouble(Double::doubleValue).average().orElse(0);
     }
 
+    /**
+     * Get Maximum Temperature
+     * 
+     * @return Maximum Temperature
+     * @see Collections#max
+     */
     public double getMaxTemperature() {
         return Collections.max(this.temperatures);
     }
 
+    /**
+     * Get Corresponding Aperture, mapping temperature to aperture value (0 - 100)
+     * 
+     * @return Corresponding Aperture
+     * @see #T1
+     * @see #T2
+     */
     public int getCorrespondingAperture() {
-        return (int) ((getCurrentTemperature() - T1) / (T2 - T1) * 100);
+        int aperture = (int) ((getCurrentTemperature() - T1) / (T2 - T1) * 100);
+
+        if(aperture < 0) {
+            return 0;
+        }
+        else if(aperture > 100) {
+            return 100;
+        }
+
+        return aperture;
     }
 }
